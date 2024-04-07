ad1af06061ceee4756d363ab602f557ce73d66eb
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-04-07 16:02:30 +0200

    day9a completed

--- day9.py
+++ day9.py
@@ -19,26 +19,41 @@ def read_input_file(file_name: str) -> list:
 
 def compute_part_one(file_name: str) -> int:
     oasis = read_input_file(file_name)
-    sum_extrapolated_values = 0
+    sum_extrapolated_values = 0  # by adding the last values of all intermediate sequence, the prediction is found
     for numbers in oasis:
-        last_numbers = [numbers[-1]]
-        print(f'{numbers= }')
-        numbers_copy = numbers
-        while len(set(numbers_copy)) > 1:
-            new_copy = []
-            for i in range(0, len(numbers_copy) - 1):
-                difference = numbers_copy[i + 1] - numbers_copy[i]
-                new_copy.append(difference)
-            numbers_copy = new_copy
-            last_numbers.append(new_copy[-1])
-            print(f'{numbers_copy= }')
-        print(f'{last_numbers= }')
-        print(f'{sum(last_numbers)= }')
-        sum_extrapolated_values += sum(last_numbers)
+        sum_extrapolated_values += numbers[-1]
+        while len(set(numbers)) > 1:  # quick way to check whether a list contains only 1 number: convert to a set
+            temp_copy_numbers = []
+            for i in range(0, len(numbers) - 1):
+                difference = numbers[i + 1] - numbers[i]
+                temp_copy_numbers.append(difference)
+            numbers = temp_copy_numbers
+            sum_extrapolated_values += temp_copy_numbers[-1]
+
+    return sum_extrapolated_values
+
+
+def compute_part_two(file_name: str) -> int:
+    oasis = read_input_file(file_name)
+    sum_extrapolated_values = 0
+    # by adding the (-1) ^depth * first values of all intermediate sequence, the prediction is found
+    # a0 = a1 - b1 + c1 - d1 ...  (a is original series, b is first differences, c i second ...
+    for numbers in oasis:
+        extrapolated_value = numbers[0]
+        depth = 0
+        while len(set(numbers)) > 1:  # quick way to check whether a list contains only 1 number: convert to a set
+            temp_copy_numbers = []
+            for i in range(0, len(numbers) - 1):
+                difference = numbers[i + 1] - numbers[i]
+                temp_copy_numbers.append(difference)
+            numbers = temp_copy_numbers
+            depth += 1
+            extrapolated_value = extrapolated_value + (-1) ** depth * temp_copy_numbers[0]
+        sum_extrapolated_values += extrapolated_value
 
     return sum_extrapolated_values
 
 
 if __name__ == '__main__':
     print(f"Part I: {compute_part_one('input/input9.txt')}")
-    # print(f"Part II: {compute_part_two('input/input8.txt')}")
+    print(f"Part II: {compute_part_two('input/input9.txt')}")
