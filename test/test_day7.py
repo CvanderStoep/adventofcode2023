d3fd45a6f1040c480a4c00224e20d4748b711b4f
Lex van der Stoep <lex.vanderstoep@gmail.com>
2024-04-08 20:14:16 +0200

    Add relative path to test
    Let's ensure that our tests can find the test resources regardless
    of what working directory they're run in.
    
    This was causing local runs of the test to fail, since they used
    a different working directory from the Workflow test runs.

--- test/test_day7.py
+++ test/test_day7.py
@@ -1,14 +1,17 @@
+import os
 import unittest
 
 from day7 import compute_part_one, compute_part_two
 
 
 class Day7Test(unittest.TestCase):
+    path = os.path.dirname(os.path.abspath(__file__))
+
     def test_input_part_one(self):
-        self.assertEqual(6440, compute_part_one('../test/input/day7.txt'))
+        self.assertEqual(6440, compute_part_one(self.path + '/input/day7.txt'))
 
     def test_input_part_two(self):
-        self.assertEqual(5905, compute_part_two('input/day7.txt'))
+        self.assertEqual(5905, compute_part_two(self.path + '/input/day7.txt'))
 
 
 if __name__ == '__main__':
