855ecc53507af62760aba681c6bfe8b67dfc5ad0
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-04-08 18:28:11 +0200

    pytest directory adjusted

--- test/test_day7.py
+++ test/test_day7.py
@@ -5,10 +5,10 @@ from day7 import compute_part_one, compute_part_two
 
 class Day7Test(unittest.TestCase):
     def test_input_part_one(self):
-        self.assertEqual(6440, compute_part_one('test/input/day7.txt'))
+        self.assertEqual(6440, compute_part_one('input/day7.txt'))
 
     def test_input_part_two(self):
-        self.assertEqual(5905, compute_part_two('test/input/day7.txt'))
+        self.assertEqual(5905, compute_part_two('input/day7.txt'))
 
 
 if __name__ == '__main__':
